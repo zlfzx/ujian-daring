3f497759eed37b45cc3e2eac3afb84fa29cf9f9d
Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2021-09-25 22:47:53 +0700

    add sidebar menu

--- resources/views/layouts/_admin_sidebar.blade.php
+++ resources/views/layouts/_admin_sidebar.blade.php
@@ -23,35 +23,87 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview menu-open">
-                    <a href="#" class="nav-link active">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                <li class="nav-header">
+                    DATA MASTER
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
                         <p>
-                            Starter Pages
-                            <i class="right fas fa-angle-left"></i>
+                            Kelas
+                            <span class="right badge badge-danger">0</span>
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link active">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Active Page</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Inactive Page</p>
-                            </a>
-                        </li>
-                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Rombel
+                            <span class="right badge badge-danger">0</span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            Siswa
+                            <span class="right badge badge-danger">0</span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-header">
+                    BANK SOAL
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-book"></i>
+                        <p>
+                            Paket Soal
+                            <span class="right badge badge-danger">0</span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-header">
+                    UJIAN
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
-                            Simple Link
-                            <span class="right badge badge-danger">New</span>
+                            Pengaturan Ujian
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Daftar Ujian
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Ujian Aktif
+                            <span class="right badge badge-danger">0</span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-header">
+                    MANAJEMEN APLIKASI
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Pengaturan
                         </p>
                     </a>
                 </li>
